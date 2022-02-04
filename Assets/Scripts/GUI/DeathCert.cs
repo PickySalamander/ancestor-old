08f32cd85a781767ac4c7e4990f6200abfb6bd6c
Joe Flaherty <joe.flaherty@selleration.com>
2022-02-04 13:28:42 -0500

    updated ignores and added initial death certificate

---
+++ Assets/Scripts/GUI/DeathCert.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace Potterblatt.GUI {
+	public class DeathCert : MonoBehaviour {
+		public Handwriting county;
+		public Handwriting state;
+		public Handwriting town;
+		public Handwriting location;
+		public Handwriting fullName;
+		public Handwriting address;
+
+#if UNITY_EDITOR
+		[ContextMenu("Sample")]
+		public void DoSample() {
+			if(!Application.isPlaying) {
+				return;
+			}
+			
+			county.Text = "The County of Fun";
+			state.Text = "New York";
+			town.Text = "Albany";
+			location.Text = "Albany Medical Center";
+			fullName.Text = "Bob Smith";
+			address.Text = "123 Ashbury Lane";
+		}
+#endif
+	}
+}
---
+++ Assets/Scripts/Globals.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Potterblatt {
+	public class Globals : MonoBehaviour {
+
+		[Header("Handwriting")]
+		public float rotationChange;
+
+		public Vector2 positionChange;
+
+		private void Awake() {
+			Instance = this;
+		}
+
+		private void OnDestroy() {
+			Instance = null;
+		}
+		
+		public static Globals Instance {
+			get;
+			private set;
+		}
+	}
+}
