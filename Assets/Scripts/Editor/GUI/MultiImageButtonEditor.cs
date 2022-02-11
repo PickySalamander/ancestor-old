b25916fc291347a5b8846f2e951c50ef52d47ef0
Joe Flaherty <joe.flaherty@selleration.com>
2022-02-11 13:52:48 -0500

    - Changed Surge to a UPM - Added page transitions - Added some organization and some Utilities

---
+++ Assets/Scripts/Editor/GUI/MultiImageButtonEditor.cs
@@ -0,0 +1,34 @@
+﻿using Potterblatt.GUI;
+using UnityEditor;
+using UnityEditor.UI;
+
+namespace Potterblatt.Editor.GUI {
+	/// <summary>
+	/// Custom Editor for the the delegating button since the <see cref="ButtonEditor"/> blocks the delegates
+	/// </summary>
+	[CustomEditor(typeof(MultiImageButton), true)]
+	[CanEditMultipleObjects]
+	public class MultiImageButtonEditor : ButtonEditor {
+		/// <summary>Property for the children</summary>
+		private SerializedProperty childrenProperty;
+
+		protected override void OnEnable() {
+			base.OnEnable();
+			
+			//setup
+			childrenProperty = serializedObject.FindProperty("children");
+		}
+
+		public override void OnInspectorGUI() {
+			base.OnInspectorGUI();
+			
+			EditorGUILayout.Space();
+			EditorGUILayout.Space();
+			
+			//Show the children
+			serializedObject.Update();
+			EditorGUILayout.PropertyField(childrenProperty);
+			serializedObject.ApplyModifiedProperties();
+		}
+	}
+}
--- Assets/Scripts/GUI/GamePage.cs
+++ Assets/Scripts/GUI/GamePage.cs
@@ -4,32 +4,24 @@ using UnityEngine;
 
 namespace Potterblatt.GUI {
 	[RequireComponent(typeof(RectTransform))]
+	[RequireComponent(typeof(CanvasGroup))]
 	public class GamePage : MonoBehaviour {
+		private CanvasGroup canvasGroup;
+		
 		private void OnEnable() {
 			
 		}
 
-		public void Show() {
-			
-			//TODO just come off right side of page and transition the other off the left
-			var rectTransform = (RectTransform) transform;
-
-			Debug.Log(PageManager.Instance.canvas.pixelRect);
-
-			var canvasRect = ((RectTransform) Manager.canvas.transform).rect;
-			var startlocation = new Vector3(canvasRect.width / 2 + Manager.newPageLocation.x,
-				-canvasRect.height / 2 + Manager.newPageLocation.y);
-			
-			Debug.Log(startlocation);
-
-			Tween.LocalPosition(rectTransform, startlocation, 
-				rectTransform.localPosition, 1, 1f);
-		}
-
-		public void Hide() {
-			
-		}
-
 		public PageManager Manager => PageManager.Instance;
+
+		public CanvasGroup CanvasGroup {
+			get {
+				if(!canvasGroup) {
+					canvasGroup = GetComponent<CanvasGroup>();
+				}
+				
+				return canvasGroup;
+			}
+		}
 	}
 }
