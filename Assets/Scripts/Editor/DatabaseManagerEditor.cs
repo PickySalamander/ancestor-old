a3bdd31f397adb0c297ec2f40b01fd78faa78682
Joe Flaherty <joe.flaherty@selleration.com>
2022-03-14 13:21:58 -0400

    added litedb and some extra editors for it

---
+++ Assets/Scripts/Editor/DatabaseManagerEditor.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using LiteDB;
+using Potterblatt.Storage;
+using Potterblatt.Storage.People;
+using UnityEditor;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+namespace Potterblatt.Editor {
+	public class DatabaseManagerEditor : EditorWindow {
+		private const string studioPref = "litedb.studio.loc";
+		
+		public string studioLocation;
+
+		private string personLookup;
+		
+		private Person editing;
+
+		private void OnEnable() {
+			studioLocation = EditorPrefs.GetString(studioPref);
+		}
+
+		private void OnGUI() {
+			UnityEngine.GUI.skin.label.wordWrap = true;
+
+			GUILayout.Label("Database Editor", EditorStyles.boldLabel);
+
+			EditorGUILayout.Space();
+
+			GUILayout.BeginHorizontal();
+
+			EditorGUILayout.TextField("Studio Location", studioLocation);
+
+			if(GUILayout.Button("Set")) {
+				studioLocation =
+					EditorUtility.OpenFilePanel("LiteDB Studio", Application.dataPath, "exe");
+
+				EditorPrefs.SetString(studioPref, studioLocation);
+			}
+
+			GUILayout.EndHorizontal();
+
+			UnityEngine.GUI.enabled = !string.IsNullOrWhiteSpace(studioLocation);
+
+			if(GUILayout.Button("Open Studio")) {
+				OpenStudio();
+			}
+
+			UnityEngine.GUI.enabled = true;
+
+			EditorGUILayout.Space();
+		}
+
+		private void OpenStudio() {
+			var pathToFile = 
+				new Uri(Path.Combine(Application.dataPath, "Resources", "database")).LocalPath;
+			
+			Debug.Log($"Opening {studioLocation} with arguments {pathToFile}");
+			
+			using var process = new Process {
+				StartInfo = {
+					FileName = studioLocation,
+					Arguments = pathToFile,
+					UseShellExecute = true,
+					RedirectStandardOutput = false,
+					WindowStyle = ProcessWindowStyle.Normal
+				}
+			};
+			
+			process.Start();
+		}
+		
+		/// <summary>Quick open function to open the window</summary>
+		[MenuItem("Ancestor/Database Editor")]
+		public static void Open() {
+			GetWindow<DatabaseManagerEditor>(false, "Database Editor", true);
+		}
+	}
+}
---
+++ Assets/Scripts/Editor/DateConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using Potterblatt.Utils;
+using UnityEditor;
+using UnityEngine;
+using Random = System.Random;
+
+namespace Potterblatt.Editor {
+	public class DateConverter : EditorWindow {
+		private string dateToConvert;
+		private string dateConverted;
+
+		private void OnGUI() {
+			UnityEngine.GUI.skin.label.wordWrap = true;
+
+			GUILayout.Label("Date Converter", EditorStyles.boldLabel);
+
+			EditorGUILayout.Space();
+
+			dateToConvert = EditorGUILayout.TextField("To Convert", dateToConvert);
+
+			EditorGUILayout.Space();
+
+			//export the animation to the asset database
+			if(GUILayout.Button("Convert")) {
+				var date = DateTime.Parse(dateToConvert, CultureInfo.InvariantCulture);
+				date = DateTime.SpecifyKind(date, DateTimeKind.Utc);
+				dateConverted = date.ToString("yyyy-MM-ddTHH:mm:ssZ");
+			}
+			
+			EditorGUILayout.Space();
+
+			EditorGUILayout.TextField(dateConverted);
+		}
+		
+		/// <summary>Quick open function to open the window</summary>
+		[MenuItem("Ancestor/Date Converter")]
+		public static void Open() {
+			GetWindow<DateConverter>(false, "Date Converter", true);
+		}
+	}
+}
