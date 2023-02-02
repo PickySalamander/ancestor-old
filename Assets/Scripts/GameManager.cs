4ab037c6d1df64025ca2ec58e267061d70216d32
Joe Flaherty <joe.flaherty@selleration.com>
2023-02-02 16:50:48 -0500

    one last documentation for game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using Potterblatt.GUI;
 using Potterblatt.Utils;
 using UnityEditor;
@@ -7,26 +5,40 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace Potterblatt {
+	/// <summary>
+	/// Manager of the whole game!
+	/// </summary>
 	public class GameManager : SingletonMonobehaviour<MonoBehaviour> {
+		[Tooltip("The exit button action")]
 		public InputActionReference quitBtn;
 
-		private bool isExiting = false;
+		/// <summary>Is the app currently exiting?</summary>
+		private bool isExiting;
 
 		private void Start() {
+			//enable the quit button
 			quitBtn.action.Enable();
 			quitBtn.action.performed += OnExit;
 		}
 
+		/// <summary>
+		/// Called when the user pushed the exit button
+		/// </summary>
+		/// <param name="callbackContext"></param>
 		private void OnExit(InputAction.CallbackContext callbackContext) {
-			if(isExiting) { return; }
-			
+			//only exit once
+			if(isExiting) {
+				return;
+			}
+
 			isExiting = true;
-			
+
+			//ask the user if they want to exit and then do it if they want
 			DialogManager.Instance.ShowDialog("Quit?",
 					"Are you sure you want to quit?", "Yes", "No").result +=
 				result => {
 					isExiting = false;
-					
+
 					if(result) {
 #if UNITY_EDITOR
 						EditorApplication.isPlaying = false;
